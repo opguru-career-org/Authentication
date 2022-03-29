b18c66dc505a747e2d0e8fef9f2262df6dcf4fff
opguru-career <102552519+opguru-career@users.noreply.github.com>
2022-03-29 22:42:18 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDfDRWuVjXaYI1iSQB9QPWNbtvwC3EH9s4",
+    authDomain: "auth-f1e1d.firebaseapp.com",
+    projectId: "auth-f1e1d",
+    storageBucket: "auth-f1e1d.appspot.com",
+    messagingSenderId: "478171339091",
+    appId: "1:478171339091:web:72570e2205b2aa6cc5a9a6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//const auth= firebase.auth();
